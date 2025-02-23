ff8c24f8b78dd45df5bdebd562670d143415bc02
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-02-23 14:40:28 -0500

    spelling: retrieve
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- js/dataTables.tableTools.js
+++ js/dataTables.tableTools.js
@@ -723,7 +723,7 @@ TableTools = function( oDT, oOpts )
 	 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 	/**
-	 * Retreieve the settings object from an instance
+	 * Retrieve the settings object from an instance
 	 *  @method fnSettings
 	 *  @returns {object} TableTools settings object
 	 */
@@ -753,7 +753,7 @@ TableTools.prototype = {
 	 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 	/**
-	 * Retreieve the settings object from an instance
+	 * Retrieve the settings object from an instance
 	 *  @returns {array} List of TR nodes which are currently selected
 	 *  @param {boolean} [filtered=false] Get only selected rows which are  
 	 *    available given the filtering applied to the table. By default
--- src/TableTools.js
+++ src/TableTools.js
@@ -288,7 +288,7 @@ TableTools = function( oDT, oOpts )
 	 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 	/**
-	 * Retreieve the settings object from an instance
+	 * Retrieve the settings object from an instance
 	 *  @method fnSettings
 	 *  @returns {object} TableTools settings object
 	 */
@@ -318,7 +318,7 @@ TableTools.prototype = {
 	 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 	/**
-	 * Retreieve the settings object from an instance
+	 * Retrieve the settings object from an instance
 	 *  @returns {array} List of TR nodes which are currently selected
 	 *  @param {boolean} [filtered=false] Get only selected rows which are  
 	 *    available given the filtering applied to the table. By default
